7d247cefd659adc16ec92f4e531bb63365cbf689
rawenloft <>
2021-03-12 20:49:12 +0200

    Add controllers and routes to  server part

---
+++ server/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+import User from '../models/user.model'
+import jwt from 'jsonwebtoken'
+import expressJwt from 'express-jwt'
+import config from './../../config/config'
+
+const signin = (req, res) => {}
+const signout = (req, res) => {}
+const requireSignin = null
+const hasAuthorization = (req, res) => {}
+
+export default { signin, signout, requireSignin, hasAuthorization }
