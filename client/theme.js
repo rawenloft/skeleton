d3dff96865aa084867424acc8cc77019ed6931ad
rawenloft <>
2021-03-15 14:25:10 +0200

    Skeleton ready for next steps

--- client/theme.js
+++ client/theme.js
@@ -1,5 +1,5 @@
 import { createMuiTheme } from '@material-ui/core/styles'
-import { pink } from '@material-ui/core/colors/pink'
+import { pink } from '@material-ui/core/colors'
 
 const theme = createMuiTheme({
     typography: {
